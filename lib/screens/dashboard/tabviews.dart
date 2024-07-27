984d47c5c20897c328898a132083720351e67ef9
Chuckibmk <tsartobnrys@gmail.com>
2024-07-27 21:55:42 +0100

    tabview update

--- lib/screens/dashboard/tabviews.dart
+++ lib/screens/dashboard/tabviews.dart
@@ -11,28 +11,28 @@ class _DashTabsState extends State<DashTabs> {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Column(
-        children: [
-          Expanded(
-            flex: 1,
-            child: Card(
-              // shape: ,
-              child: Column(
-                children: [
-                  Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: 200,
-                    child: Image.asset(
-                      'assets/logo/DT_logo.png',
-                      fit: BoxFit.fill,
-                    ),
-                  ),
-                ],
+        // child: Column(
+        //   children: [
+        child: Expanded(
+      flex: 1,
+      child: Card(
+        // shape: ,
+        child: Column(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 200,
+              child: Image.asset(
+                'assets/logo/DT_logo.png',
+                fit: BoxFit.fill,
               ),
             ),
-          )
-        ],
+          ],
+        ),
       ),
-    );
+    )
+        //   ],
+        // ),
+        );
   }
 }
