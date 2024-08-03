85f0b1700e6bb0a92e53c82361a855d9ac69d054
Chuckibmk <tsartobnrys@gmail.com>
2024-08-03 21:10:08 +0100

    search update

--- lib/screens/search/search.dart
+++ lib/screens/search/search.dart
@@ -1,6 +1,6 @@
 import 'package:api_news/screens/account/signin.dart';
 import 'package:api_news/screens/account/signup.dart';
-import 'package:flutter/gestures.dart';
+// import 'package:flutter/gestures.dart';
 // import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 // import 'package:flutter/widgets.dart';
